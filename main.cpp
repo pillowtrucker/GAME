9e887cbb08b52e7bbd3d0c2e835e04bad429cce4
Mauritius Clemens <githubino@janitor.chat>
2023-11-25 12:08:14 +0100

    reexport models to obj.. fix up all the blender jank and the jank of this engine, move therac minigame to its own module

--- main.cpp
+++ main.cpp
@@ -1,90 +1,58 @@
-# include "Siv3D.hpp"
-#include "UnfriendlyTextBox.h"
-#include "TheracConfig.h"
+# include <Siv3D.hpp>
+#include "TheracMinigame.h"
 
 void Main()
 {
-    auto background_colour = Palette::Black;
-    auto main_monitor = System::GetCurrentMonitor();
-    auto const mms = main_monitor.fullscreenResolution;
-    auto const test_font_size = 100;
-
-    Font test_font{FontMethod::MSDF, test_font_size, U"resources/engine/font/hasklug/Hasklug.otf"};
-    auto test_font_px_width = test_font.getGlyphInfo(U"█").width;
-    String longest_column{U"COLLIMATOR ROTATION(DEG)"};
-    ColorF transparent{1,1,1,0};
-    double num_columns = 7;
-    double column_width = Math::Floor(mms.x/num_columns);
-    auto pixel_budget = column_width/longest_column.length();
-    auto pixels_per_point = static_cast<double>(test_font_px_width)/test_font_size;
-    auto const font_size = Math::Floor(pixel_budget/pixels_per_point);
-    
-    Font myMonoFont{FontMethod::MSDF, static_cast<uint16_t>(font_size), U"resources/engine/font/hasklug/Hasklug.otf"};
-    Array<double> columns_ = Array<double>(num_columns,column_width);
-    SimpleTable grid
-    {
-        (Array<double> const &) columns_,
-        {
-            .borderThickness = 0, .backgroundColor = background_colour,.textColor = Palette::Lime, .variableWidth = false, .font = myMonoFont, .fontSize = font_size
-        }
-    };
     int32 fps;
+    Window::Resize(1920, 1080);
+	const ColorF backgroundColor = ColorF{ 0.4, 0.6, 0.8 }.removeSRGBCurve();
+    const Mesh groundPlane{ MeshData::OneSidedPlane(2000, { 400, 400 }) };
+    const Model theracModel{ U"resources/therac-25-base.obj" };
+    Model pdp11Model{ U"resources/pdp11.obj" };
+    const Model vt100Model{ U"resources/vt100.obj" };
     
-    Window::Resize(mms);
-    Window::Maximize();
-    Scene::SetBackground(background_colour);
-    namespace tc = TheracConfig;
-    Deserializer<BinaryReader> reader(U"resources/therac_ui.bin");
-    Array<Array<std::pair<String,tc::TheracTextType>>> ui_widgets;
-    reader(ui_widgets);
-
-    HashTable<String, tc::TheracTextType> widget_types;
-    
-    ui_widgets.each([&grid,&widget_types] (Array<std::pair<String,tc::TheracTextType>> row){
-        auto label_row = row.map([&widget_types](std::pair<String,tc::TheracTextType> cell)
-        {
-            widget_types.insert(cell);
-            return cell.first;
-        });
-
-        grid.push_back_row(label_row);
-    });
-    auto actual_row_height = grid.height()/grid.rows();
-    HashTable<String, tc::TheracConfig*> dynamic_widgets;
-
-    grid.items().each_index([&widget_types,&dynamic_widgets,&grid,transparent](auto i, auto v)
-    {    
-        if(v.text.starts_with(U"PL") )
-        {
-            auto name = v.text;
-            TextEditState tes;
-            tc::TheracConfig * thc = new tc::TheracConfig{name,i,grid,tes,widget_types};
-                
-            dynamic_widgets.insert({name,thc});
-            grid.setTextColor(i.y,i.x,transparent);
-            grid.setBackgroundColor(i.y,i.x,transparent);
-        }
-    });
-    for (auto w: dynamic_widgets)
-    {
-        w.second->dynamic_widgets = dynamic_widgets;
-        w.second->finish_setup();
-    }
-   
+    Model::RegisterDiffuseTextures(theracModel, TextureDesc::MippedSRGB);
+    Model::RegisterDiffuseTextures(vt100Model, TextureDesc::MippedSRGB);
+    Model::RegisterDiffuseTextures(pdp11Model, TextureDesc::MippedSRGB);
+    const MSRenderTexture renderTexture{ Scene::Size(), TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes };
+	DebugCamera3D camera{ Graphics3D::GetRenderTargetSize(), 40_deg, Vec3{ 0, 3, -16 } };
+    const Texture groundTexture{ U"resources/PolyPackBoats.png", TextureDesc::MippedSRGB };
 	while (System::Update())
 	{
-        grid.draw(Vec2{0,0});
-        grid.items().each_index([&dynamic_widgets,background_colour,column_width,actual_row_height,&myMonoFont](auto i, auto v)
-        {
-            if(v.text.starts_with(U"PL"))
-            {
-                tc::TheracConfig & w = *dynamic_widgets[v.text];
-                mine::UnfriendlyTextBox::TextBox(w.tes, Vec2{ i.x * column_width, i.y * actual_row_height},column_width,w.max_chars,w.enabled,myMonoFont,background_colour,actual_row_height);
-                w.mangle();
+        camera.update(4.0);
+		Graphics3D::SetCameraTransform(camera);
 
-            }
-            });
+		// [3D シーンの描画]
+		{
+			const ScopedRenderTarget3D target{ renderTexture.clear(backgroundColor) };
+
+			// [モデルの描画]
+			{
+				// 地面の描画
+				groundPlane.draw(groundTexture);
+
+				// 球の描画
+                //				Sphere{ { 0, 1, 0 }, 1 }.draw(ColorF{ 0.75 }.removeSRGBCurve());
+                vt100Model.draw(Vec3{0,0,0});
+                pdp11Model.draw(Vec3{5,0,-5});
+                
+				theracModel.draw(Vec3{ 2, 0, -2 }, Quaternion::RotateY(180_deg));
+			}
+		}
+
+		// [RenderTexture を 2D シーンに描画]
+		{
+			Graphics3D::Flush();
+			renderTexture.resolve();
+            
+			Shader::LinearToScreen(renderTexture);
+		}
+        if(KeyF5.up())
+            goto Therac;
 		fps = Profiler::FPS();
 		Window::SetTitle(fps);
+        
 	}
+ Therac:
+    TheracMinigame();
 }
---
+++ src/TheracMinigame.cpp
@@ -0,0 +1,91 @@
+#include <Siv3D.hpp>
+#include "TheracConfig.h"
+#include "UnfriendlyTextBox.h"
+void TheracMinigame() {
+  auto background_colour = Palette::Black;
+  auto main_monitor = System::GetCurrentMonitor();
+  auto const mms = main_monitor.fullscreenResolution;
+  auto const test_font_size = 100;
+
+  Font test_font{FontMethod::MSDF, test_font_size,
+                 U"resources/engine/font/hasklug/Hasklug.otf"};
+  auto test_font_px_width = test_font.getGlyphInfo(U"█").width;
+  String longest_column{U"COLLIMATOR ROTATION(DEG)"};
+  ColorF transparent{1, 1, 1, 0};
+  double num_columns = 7;
+  double column_width = Math::Floor(mms.x / num_columns);
+  auto pixel_budget = column_width / longest_column.length();
+  auto pixels_per_point =
+      static_cast<double>(test_font_px_width) / test_font_size;
+  auto const font_size = Math::Floor(pixel_budget / pixels_per_point);
+
+  Font myMonoFont{FontMethod::MSDF, static_cast<uint16_t>(font_size),
+                  U"resources/engine/font/hasklug/Hasklug.otf"};
+  Array<double> columns_ = Array<double>(num_columns, column_width);
+  SimpleTable grid{(Array<double> const &)columns_,
+                   {.borderThickness = 0,
+                    .backgroundColor = background_colour,
+                    .textColor = Palette::Lime,
+                    .variableWidth = false,
+                    .font = myMonoFont,
+                    .fontSize = font_size}};
+  int32 fps;
+
+  Window::Resize(mms);
+  Window::Maximize();
+  Scene::SetBackground(background_colour);
+  namespace tc = TheracConfig;
+  Deserializer<BinaryReader> reader(U"resources/therac_ui.bin");
+  Array<Array<std::pair<String, tc::TheracTextType>>> ui_widgets;
+  reader(ui_widgets);
+
+  HashTable<String, tc::TheracTextType> widget_types;
+
+  ui_widgets.each([&grid, &widget_types](
+                      Array<std::pair<String, tc::TheracTextType>> row) {
+    auto label_row =
+        row.map([&widget_types](std::pair<String, tc::TheracTextType> cell) {
+          widget_types.insert(cell);
+          return cell.first;
+        });
+
+    grid.push_back_row(label_row);
+  });
+  auto actual_row_height = grid.height() / grid.rows();
+  HashTable<String, tc::TheracConfig *> dynamic_widgets;
+
+  grid.items().each_index(
+      [&widget_types, &dynamic_widgets, &grid, transparent](auto i, auto v) {
+        if (v.text.starts_with(U"PL")) {
+          auto name = v.text;
+          TextEditState tes;
+          tc::TheracConfig *thc =
+              new tc::TheracConfig{name, i, grid, tes, widget_types};
+
+          dynamic_widgets.insert({name, thc});
+          grid.setTextColor(i.y, i.x, transparent);
+          grid.setBackgroundColor(i.y, i.x, transparent);
+        }
+      });
+  for (auto w : dynamic_widgets) {
+    w.second->dynamic_widgets = dynamic_widgets;
+    w.second->finish_setup();
+  }
+
+  while (System::Update()) {
+    grid.draw(Vec2{0, 0});
+    grid.items().each_index([&dynamic_widgets, background_colour, column_width,
+                             actual_row_height, &myMonoFont](auto i, auto v) {
+      if (v.text.starts_with(U"PL")) {
+        tc::TheracConfig &w = *dynamic_widgets[v.text];
+        mine::UnfriendlyTextBox::TextBox(
+            w.tes, Vec2{i.x * column_width, i.y * actual_row_height},
+            column_width, w.max_chars, w.enabled, myMonoFont, background_colour,
+            actual_row_height);
+        w.mangle();
+      }
+    });
+    fps = Profiler::FPS();
+    Window::SetTitle(fps);
+  }
+}
